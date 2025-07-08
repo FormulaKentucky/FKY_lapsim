1f0558244f0cf1ec736b12441e75ba23deb66224
FormulaKentucky <formulakentucky@gmail.com>
2025-07-08 18:46:09 -0400

    Update lapsim.m

--- FK08_Lapsim/lapsim.m
+++ FK08_Lapsim/lapsim.m
@@ -5,9 +5,15 @@ close all hidden
 %% Import Car/Data
 fprintf('Select a vehicle file.\n')
 carname = uigetfile('*.csv');
-disp('Selected: ' + carname)
+%disp('Selected: ' + carname)
 
 fprintf('Select a track file. \n')
 trackname = uigetfile('*csv');
-disp('Selected: ' + trackname)
+%disp('Selected: ' + trackname)
 
+car = readtable(carname, 'VariableNamingRule', 'preserve');
+track = readtable(trackname, 'VariableNamingRule', 'preserve');
+
+%% Maximum Cornering Velocity Lookup Table
+syms mu m g rho a cl v cd r cr
+eqn = (mu*((m*g)+(0.5*rho*a*cl*v^2)))^2 == (((cr*((m*g) + (0.5*rho*a*cl*v^2))) + (0.5*rho*a*cd*v^2))^2) + ((m*v^2)/r)^2
