8c3ce1449c59c1f64f344543d62c699fbe188d8b
FormulaKentucky <formulakentucky@gmail.com>
2025-07-08 18:57:49 -0400

    Update lapsim.m

--- FK08_Lapsim/lapsim.m
+++ FK08_Lapsim/lapsim.m
@@ -16,18 +16,24 @@ track = readtable(trackname, 'VariableNamingRule', 'preserve');
 
 %% Maximum Cornering Velocity Lookup Table
 syms mu m g rho a cl v cd r cr
-eqn = (mu*((m*g)+(0.5*rho*a*cl*v^2)))^2 == (((cr*((m*g) + (0.5*rho*a*cl*v^2))) + (0.5*rho*a*cd*v^2))^2) + ((m*v^2)/r)^2
+eqn = (mu*((m*g)+(0.5*rho*a*cl*v^2)))^2 == (((cr*((m*g) + (0.5*rho*a*cl*v^2))) + (0.5*rho*a*cd*v^2))^2) + ((m*v^2)/r)^2;
 sol = solve(eqn, v);
 vars = [mu, m, g, rho, a, cl, cd, cr, r];
 
-mu = car{4,9}
-m = car{3,11}
-g = car{2,11}
-rho = car{1,11}
-a = car{3,10}
-cl = car{1,10}
-cd = car{11,8}
+mu = car{4,9};
+m = car{3,11};
+g = car{2,11};
+rho = car{1,11};
+a = car{3,10};
+cl = car{1,10};
+cd = car{11,8};
 
 parameters = [mu, m, g, rho, a, cl, cd, cr];
 vmax = zeros(numel(track(:,3)),1);
+
+for i = 1:numel(track(:,3))
+    parameters(9) = track{i, 3};
+    vmax(i) = double(max(abs(vpa(subs(sol, vars, parameters))))); 
+end
+
 
